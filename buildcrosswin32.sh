fdb9c7f0a1d827933d5b059d57ba750dcc57b8c2
Timo Boegner <tboegner@googlemail.com>
2021-11-17 18:32:23 +0100

    Updated SDL libarier: SDL -> 2.0.14, SDL-mixer -> 2.0.4, SDL-ttf -> 2.0.15. Dunelegacy -> 0.97.0

--- buildcrosswin32.sh
+++ buildcrosswin32.sh
@@ -5,42 +5,64 @@ mkdir -p build
 mkdir -p build/x86
 if [ ! -e build/x86/SDL2.dll ]
 then
-	echo "Downloading 32-bit SDL2.dll..."
-	wget -c http://libsdl.org/release/SDL2-2.0.5-win32-x86.zip -O build/x86/SDL2-2.0.5-win32-x86.zip
-	unzip -q build/x86/SDL2-2.0.5-win32-x86.zip -d build/x86 SDL2.dll
-	rm build/x86/SDL2-2.0.5-win32-x86.zip
+	ZIP_FILE=SDL2-2.0.14-win32-x86.zip
+	echo "Downloading 32-bit ${ZIP_FILE}..."
+	wget -c http://libsdl.org/release/${ZIP_FILE} -O build/x86/${ZIP_FILE}
+	unzip -q build/x86/${ZIP_FILE} -d build/x86 SDL2.dll
+	rm build/x86/${ZIP_FILE}
 fi
 
 if [ ! -e build/x86/SDL2_mixer.dll ]
 then
-	echo "Downloading 32-bit SDL2_mixer.dll..."
-	wget -c https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1-win32-x86.zip -O build/x86/SDL2_mixer-2.0.1-win32-x86.zip
-	unzip -q build/x86/SDL2_mixer-2.0.1-win32-x86.zip -d build/x86 SDL2_mixer.dll smpeg2.dll libvorbisfile-3.dll libvorbis-0.dll libogg-0.dll libFLAC-8.dll libmodplug-1.dll
-	rm build/x86/SDL2_mixer-2.0.1-win32-x86.zip
+	ZIP_FILE=SDL2_mixer-2.0.4-win32-x86.zip
+	echo "Downloading 32-bit ${ZIP_FILE}..."
+	wget -c https://www.libsdl.org/projects/SDL_mixer/release/${ZIP_FILE} -O build/x86/${ZIP_FILE}
+	unzip -q build/x86/${ZIP_FILE} -d build/x86 SDL2_mixer.dll libmpg123-0.dll libvorbisfile-3.dll libvorbis-0.dll libogg-0.dll libFLAC-8.dll libmodplug-1.dll
+	rm build/x86/${ZIP_FILE}
+fi
+
+if [ ! -e build/x86/SDL2_ttf.dll ]
+then
+	ZIP_FILE=SDL2_ttf-2.0.15-win32-x86.zip
+	echo "Downloading 32-bit ${ZIP_FILE}..."
+	wget -c https://www.libsdl.org/projects/SDL_ttf/release/${ZIP_FILE} -O build/x86/${ZIP_FILE}
+	unzip -q build/x86/${ZIP_FILE} -d build/x86 SDL2_ttf.dll libfreetype-6.dll zlib1.dll
+	rm build/x86/${ZIP_FILE}
 fi
 
 mkdir -p build/x64
 if [ ! -e build/x64/SDL2.dll ]
 then
-	echo "Downloading 64-bit SDL2.dll..."
-	wget -c http://libsdl.org/release/SDL2-2.0.5-win32-x64.zip -O build/x64/SDL2-2.0.5-win32-x64.zip
-	unzip -q build/x64/SDL2-2.0.5-win32-x64.zip -d build/x64 SDL2.dll
-	rm build/x64/SDL2-2.0.5-win32-x64.zip
+	ZIP_FILE=SDL2-2.0.14-win32-x64.zip
+	echo "Downloading 64-bit ${ZIP_FILE}..."
+	wget -c http://libsdl.org/release/${ZIP_FILE} -O build/x64/${ZIP_FILE}
+	unzip -q build/x64/${ZIP_FILE} -d build/x64 SDL2.dll
+	rm build/x64/${ZIP_FILE}
 fi
 
 if [ ! -e build/x64/SDL2_mixer.dll ]
 then
-	echo "Downloading 64-bit SDL2_mixer.dll..."
-	wget -c https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1-win32-x64.zip -O build/x64/SDL2_mixer-2.0.1-win32-x64.zip
-	unzip -q build/x64/SDL2_mixer-2.0.1-win32-x64.zip -d build/x64 SDL2_mixer.dll smpeg2.dll libvorbisfile-3.dll libvorbis-0.dll libogg-0.dll libFLAC-8.dll libmodplug-1.dll
-	rm build/x64/SDL2_mixer-2.0.1-win32-x64.zip
+	ZIP_FILE=SDL2_mixer-2.0.4-win32-x64.zip
+	echo "Downloading 64-bit ${ZIP_FILE}..."
+	wget -c https://www.libsdl.org/projects/SDL_mixer/release/${ZIP_FILE} -O build/x64/${ZIP_FILE}
+	unzip -q build/x64/${ZIP_FILE} -d build/x64 SDL2_mixer.dll libmpg123-0.dll libvorbisfile-3.dll libvorbis-0.dll libogg-0.dll libFLAC-8.dll libmodplug-1.dll
+	rm build/x64/${ZIP_FILE}
+fi
+
+if [ ! -e build/x64/SDL2_ttf.dll ]
+then
+	ZIP_FILE=SDL2_ttf-2.0.15-win32-x64.zip
+	echo "Downloading 64-bit ${ZIP_FILE}..."
+	wget -c https://www.libsdl.org/projects/SDL_ttf/release/${ZIP_FILE} -O build/x64/${ZIP_FILE}
+	unzip -q build/x64/${ZIP_FILE} -d build/x64 SDL2_ttf.dll libfreetype-6.dll zlib1.dll
+	rm build/x64/${ZIP_FILE}
 fi
 
 autoreconf --install
 mkdir -p build/src
 i686-w64-mingw32-windres -o build/src/resource.o resource.rc
 cd build
-../configure --prefix="" --host=i686-w64-mingw32 --with-sdl-prefix=/usr/i686-w64-mingw32 --disable-sdltest && make $1 && cp src/dunelegacy.exe x86/
+../configure --prefix="" --host=i686-w64-mingw32 --with-sdl-prefix=/usr/local/sdl/i686-w64-mingw32 --disable-sdltest && make $1 && cp src/dunelegacy.exe x86/
 cd ..
 
 cd build
@@ -51,7 +73,7 @@ autoreconf --install
 mkdir -p build/src
 x86_64-w64-mingw32-windres -o build/src/resource.o resource.rc
 cd build
-../configure --prefix="" --host=x86_64-w64-mingw32 --with-sdl-prefix=/usr/x86_64-w64-mingw32 --disable-sdltest && make $1 && cp src/dunelegacy.exe x64/
+../configure --prefix="" --host=x86_64-w64-mingw32 --with-sdl-prefix=/usr/local/sdl/x86_64-w64-mingw32 --disable-sdltest && make $1 && cp src/dunelegacy.exe x64/
 cd ..
 
 makensis dunelegacy.nsi
